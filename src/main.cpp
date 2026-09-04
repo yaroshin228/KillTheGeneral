a931036e5ba7b792dd32b94df06c95412bbc5c34
Ярослав <yyyarick2012@gmail.com>
2026-09-04 14:06:50 +0500

    style: remove trailing spaces and replace endl with newline

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 #include <random>
 
-using namespace std;
+//using namespace std;
 
 int main() {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 2);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 2);
 
     int general_choice = dist(gen);
     int guess;
 
-    cout << "Привет! Ты в игре 'Убей генерала'" << endl;
-    cout << "Твоя задача подсыпать яд в кружку которую выпьет генерал и убить его!" << endl;
-    cout << "Но если он выберет не отравленную кружку ты проиграешь!" << endl;
-    cout << "Нажми Enter для продолжения..." << endl;
+    std::cout << "Привет! Ты в игре 'Убей генерала'.\n";
+    std::cout << "Твоя задача подсыпать яд в кружку которую выпьет генерал и убить его!\n";
+    std::cout << "Но если он выберет не отравленную кружку ты проиграешь!\n";
+    std::cout << "Нажми Enter для продолжения...\n";
 
-    cin.get();
+    std::cin.get();
 
-    cout << "Генерал скоро будет!" << endl;
-    cout << "Выбери кружку для отравления (1 или 2)." << endl;
+    std::cout << "Генерал скоро будет!\n";
+    std::cout << "Выбери кружку для отравления (1 или 2).\n";
 
-    cin >> guess;
+    std::cin >> guess;
 
     if (guess == general_choice) {
-        cout << "Генерал отравлен, \033[32mты победил!\033[0m";
+        std::cout << "Генерал отравлен, \033[32mты победил!\033[0m";
     }
 
     else {
-        cout << "\033[31mТы проиграл!\033[0m Генерал выбрал не отравленную кружку";
+        std::cout << "\033[31mТы проиграл!\033[0m Генерал выбрал не отравленную кружку...";
     }
-
     return 0;
-
-    }
+}
